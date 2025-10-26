d935e3decf5024bee94b9c96412f185f36071efe
Tomás Palma <59887569+tomaspalma@users.noreply.github.com>
2025-10-26 15:43:39 +0000

    Refactor position tracking to include multiple robots

--- controllers/position_track/position_track.py
+++ controllers/position_track/position_track.py
@@ -1,35 +1,44 @@
-from controller import Supervisor
 import csv
+from controller import Supervisor
 
 supervisor = Supervisor()
-
 timestep = int(supervisor.getBasicTimeStep())
 
 root = supervisor.getRoot()
 children_field = root.getField("children")
 
-robot_node = None
+follower_node = None
+myrobot_node = None
 
 for i in range(children_field.getCount()):
     child = children_field.getMFNode(i)
     if child.getField("name") is not None:
         name_field = child.getField("name")
         if name_field.getSFString() == "my_robot":
-            robot_node = child
+            myrobot_node = child
+        elif name_field.getSFString() == "follower":
+            follower_node = child
 
-translation_field = robot_node.getField("translation")
+follower_translation = follower_node.getField("translation")
+myrobot_translation = myrobot_node.getField("translation")
 
-with open('robot_position.csv', mode='w', newline='') as file:
+with open('robot_positions.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['time (s)', 'x', 'y'])
+    writer.writerow(['time (s)', 
+                     'follower_x', 'follower_y', 
+                     'myrobot_x', 'myrobot_y'])
 
     time = 0.0
     while supervisor.step(timestep) != -1:
-        pos = translation_field.getSFVec3f()
+        follower_pos = follower_translation.getSFVec3f()
+        myrobot_pos = myrobot_translation.getSFVec3f()
 
-        x, y = pos[0], pos[2]
+        follower_x, follower_y = follower_pos[0], follower_pos[1]
+        myrobot_x, myrobot_y = myrobot_pos[0], myrobot_pos[1]
 
-        writer.writerow([time / 1000.0, x, y])
+        writer.writerow([time / 1000.0, 
+                         follower_x, follower_y, 
+                         myrobot_x, myrobot_y])
         file.flush()
 
         time += timestep
